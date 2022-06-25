74a4a10971e0a00d0c8baaf285e09a475b34f577
iamrajabli <iamrajabli@outlook.com>
2022-06-26 01:30:46 +0400

    Added Context API [this.context]

--- 01BooksProject/src/components/BookList.jsx
+++ 01BooksProject/src/components/BookList.jsx
@@ -8,22 +8,22 @@ export default class BookList extends Component {
         const { books } = this.props;
 
         const bookList = books.map((book, i) => {
-            return <Book books={book} key={i}/>
+            return <Book books={book} key={i} />
         })
         return (
             <section className="page-section bg-light" id="portfolio">
-            <div className="container">
-                <div className="text-center">
-                    <h2 className="section-heading text-uppercase">BookFolio</h2>
-                    <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                </div>
-                <div className="row">
+                <div className="container">
+                    <div className="text-center">
+                        <h2 className="section-heading text-uppercase">BookFolio</h2>
+                        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    </div>
+                    <div className="row">
 
-                    {bookList}
+                        {bookList}
 
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
         )
     }
 }
