adbc1ee5633401848502ff97868e9447c7f4fada
iamrajabli <iamrajabli@outlook.com>
2022-06-27 15:35:01 +0400

    Added to project multiple consumer

--- 01BooksProject/src/components/BookList/BookList.jsx
+++ 01BooksProject/src/components/BookList/BookList.jsx
@@ -12,14 +12,20 @@ export default class BookList extends Component {
                 {themeContextValue => (
                     <BookContext.Consumer>
                         {bookContextValue => {
-                            const { isDarkTheme, dark, light } = themeContextValue;
+                            const { isDarkTheme, dark, light, changeTheme } = themeContextValue;
                             const theme = isDarkTheme ? dark : light;
+                            const classChangeBtn = isDarkTheme ? 'btn btn-warning mb-4' : 'btn btn-dark mb-4'
                             return (
-                                <section className="page-section" style={{background: theme.bg, color:theme.txt}} id="portfolio">
+                                <section className="page-section" style={{ background: theme.bg, color: theme.txt }} id="portfolio">
                                     <div className="container">
                                         <div className="text-center">
                                             <h2 className="section-heading text-uppercase">BookFolio</h2>
                                             <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                                            <button
+                                                onClick={() => changeTheme()}
+                                                type="button"
+                                                className={classChangeBtn}>
+                                                Change Theme</button>
                                         </div>
                                         <div className="row">
 
